7c3d84ae2673477e9d7aee8fa4f286db40b8b8c9
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2025-03-14 15:54:33 -0300

    Add project files.

---
+++ rede-social-c#/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace rede_social_c_.Entities
+{
+    class Comment
+    {
+        public string? Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ rede-social-c#/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using rede_social_c_.Entities;
+
+namespace rede_social_c_
+{
+ class Program
+{
+    static void Main(string[] args)
+    {
+
+     Comment c1 = new Comment("Have a nice trip");
+     Comment c2 = new Comment("Wow that's awesome");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2025 13:05:44"),
+                "Travelling to New Zeeland",
+                "I'm going to visit this wonderful country",
+                50
+               );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("Good dreams");
+
+            Post p2 = new Post(
+                DateTime.Parse("21/06/2025 13:05:44"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+                );
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
